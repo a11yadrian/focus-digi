46a53fc707841f0f98a99eedcf02aba21ddc9908
a11yadrian <adrian.wegener@gmx.de>
2024-06-05 08:45:59 +0200

    smaller result.js

--- models/result.js
+++ models/result.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const resultSchema = new mongoose.Schema({
   result: String,
-  timestamp: { type: Date, default: Date.now }
 }); 
 
 module.exports = mongoose.models.Result || mongoose.model('Result', resultSchema);

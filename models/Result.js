9dbb762d1f7dfbed2202dc09397a00504a082b58
a11yadrian <adrian.wegener@gmx.de>
2024-06-05 10:46:08 +0200

    schema without timestamp

--- models/Result.js
+++ models/Result.js
@@ -1,18 +1,8 @@
 const mongoose = require('mongoose');
 
 const resultSchema = new mongoose.Schema({
-  result: {
-    type: String,
-    required: true
-  },
-  time: {
-    type: Number,
-    required: true
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
+  result: String,
+  time: Number,
 }); 
 
 module.exports = mongoose.models.Result || mongoose.model('Result', resultSchema);

916f12680fbbfef12ca426b74b8338210c841148
a11yadrian <adrian.wegener@gmx.de>
2024-06-05 10:44:17 +0200

    update mongoose schema

--- models/Result.js
+++ models/Result.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const resultSchema = new mongoose.Schema({
-  result: String,
+  result: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: Number,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 }); 
 
 module.exports = mongoose.models.Result || mongoose.model('Result', resultSchema);
